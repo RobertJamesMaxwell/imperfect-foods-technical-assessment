913e23b1986c3569dd54a23cd974d2d0034dd1fe
Robert Maxwell <robertjamesmaxwell@gmail.com>
2020-08-05 22:38:35 -0500

    Added cors for local development. Made network call to fetch products data from server.

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const products = require('../src/products.json');
+const express = require("express");
+const products = require("../src/products.json");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 9001;
 
-app.get('/products', (req, res) => {
+app.use(cors()); //TODO enable only in dev mode
+
+app.get("/products", (req, res) => {
   return res.json(products);
 });
 
--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import './App.css';
-import Products from './Products';
-import products from './products.json';
-import Filters from './Filters';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Products from "./Products";
+import Filters from "./Filters";
 
-const App = () => (
-  <div className="app">
-    <div className="App-row">
-      <Filters />
+// TODO This should ideally come from environment variables
+const localProductsUrl = "http://localhost:9001/products";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(localProductsUrl)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("res", response);
+        setProducts(response);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className='app'>
+      <div className='App-row'>
+        <Filters />
+      </div>
+      <div className='App-row'>
+        <Products products={products} />
+      </div>
     </div>
-    <div className="App-row">
-      <Products products={products} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default App;
